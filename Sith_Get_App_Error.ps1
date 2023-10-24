52ac88beaa12ac4d767788cae6177fd72e142eb6
Pfranco74 <148639529+Pfranco74@users.noreply.github.com>
2023-10-24 14:47:30 +0100

    Add files via upload

---
+++ Sith_Get_App_Error.ps1
@@ -0,0 +1,32 @@
+﻿cls
+$AllApp = $null
+$readfile = Get-Content -Path C:\data\IntuneManagementExtension.log
+write-host "Apps to be install " -ForegroundColor Green
+foreach ($item in $readfile)
+{
+    if ($item -like "* In EspPhase: DeviceSetup. App*")
+    {
+        $AppID = $item.Substring(60,40)
+        $AppNAme = ($item.Substring(128)).split("]")[0]
+        $AppInst = "AppID: " + $AppID + " AppName: " + $AppNAme       
+        $AllApp = @($AppInst) + $AllApp
+        Write-Output $AppInst    
+    }
+
+    $erro = "NewValue"":""Error"
+    if ($item -like "*$erro*")
+    {
+         $foundError = $item.Substring(69,36)
+    }
+}
+
+foreach ($item in $AllApp)
+{
+    if ($item -like "*$foundError*")
+    {
+        write-host "Found error "  -ForegroundColor Red
+        write-host $item
+    }
+}
+
+#MBCP - CORE - AutoBranding), start downloading..

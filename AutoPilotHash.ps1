c262197b412ed6231edaf649d56bd1f135dae11f
Pfranco74 <148639529+Pfranco74@users.noreply.github.com>
2023-11-09 10:36:53 +0000

    Add files via upload

---
+++ AutoPilotHash.ps1
@@ -0,0 +1,7 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+New-Item -Type Directory -Path "D:\HASH"
+Set-Location -Path "D:\HASH"
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-Script -Name Get-WindowsAutopilotInfo
+Get-WindowsAutopilotInfo -OutputFile AutopilotHWID.csv

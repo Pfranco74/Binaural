5e953fc888ef920a405216bae4e744d12e7f9c49
Pfranco74 <148639529+Pfranco74@users.noreply.github.com>
2024-04-29 13:42:20 +0100

    Delete Inst.cmd

--- Inst.cmd
+++
@@ -1,9 +0,0 @@
-if not exist c:\Windows\Temp\Logs\PKI md c:\Windows\Temp\Logs\PKI
-if not exist c:\temp md c:\Temp
-if not exist c:\Windows\Temp\Logs\AutoPilot md c:\Windows\Temp\Logs\AutoPilot
-
-echo Begin %date% %time% >> c:\Windows\Temp\Logs\AutoPilot\PKI.log
-
-msiexec.exe /i "Setup.Management.v.2.0.msi" /quiet /norestart /l*v c:\Windows\Temp\Logs\PKI\pki_inst.log
-
-echo End %date% %time% >> c:\Windows\Temp\Logs\AutoPilot\PKI.log

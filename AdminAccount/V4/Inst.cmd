f216d5a9d7df40e489825f572b8b6164cc38727d
Pfranco74 <148639529+Pfranco74@users.noreply.github.com>
2024-05-07 16:52:18 +0100

    Delete AdminAccount/V4 directory

--- AdminAccount/V4/Inst.cmd
+++
@@ -1,8 +0,0 @@
-if not exist c:\Windows\Temp\Logs\AutoPilot md c:\Windows\Temp\Logs\AutoPilot
-
-echo Begin %date% %time% >> c:\Windows\Temp\Logs\AutoPilot\Sith_AdminAcoount.log
-powershell -ExecutionPolicy Bypass -file Sith_AdminAcoount.ps1
-
-:end
-echo End %date% %time% >> c:\Windows\Temp\Logs\AutoPilot\Sith_AdminAcoount.log
-EXIT %EXITCODE%

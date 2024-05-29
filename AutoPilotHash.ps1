18e1ce948f1c826eabf3d82d39cd18ae432289ab
Pfranco74 <148639529+Pfranco74@users.noreply.github.com>
2024-05-29 21:56:07 +0100

    Update AutoPilotHash.ps1

--- AutoPilotHash.ps1
+++ AutoPilotHash.ps1
@@ -1,7 +1,12 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-New-Item -Type Directory -Path "D:\HASH"
+
+if (-not (Test-Path "D:\HASH"))
+{
+  Mkdir "D:\HASH"
+}
+
 Set-Location -Path "D:\HASH"
 $env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
-Install-Script -Name Get-WindowsAutopilotInfo
+Install-Script -Name Get-WindowsAutopilotInfo -force
 Get-WindowsAutopilotInfo -OutputFile AutopilotHWID.csv

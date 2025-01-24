b2fb4a159e6e67b048a2f2df7e8b2dd7d2a65fb1
Pfranco74 <148639529+Pfranco74@users.noreply.github.com>
2025-01-24 09:57:33 +0000

    Add files via upload

---
+++ Sith_HPAC.ps1
@@ -0,0 +1,262 @@
+﻿# HP Accessory Center Link
+# https://apps.microsoft.com/detail/9P87FCQVTC59?hl=en-us&gl=PT&ocid=pdpshare
+Remove-Variable * -ErrorAction SilentlyContinue
+
+$scriptVersion = "20250123"
+
+function ForceErr
+{
+    Stop-Transcript
+    Rename-Item -Path $LogFile -NewName $LogErr -Force
+    
+    if ((Test-Path $LogFile))
+    {
+        Remove-Item -Path $LogFile -Force
+    }
+
+    $intunelogerr = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\IntuneManagementExtension-" + $Logerr.Split("\")[-1] + ".log"
+    Copy-Item $Logerr $intunelogerr -Force -ErrorAction SilentlyContinue
+    
+
+    exit 13    
+}
+
+function CreateDir ($param1)
+{
+    try
+    {
+        if (-not (Test-Path $param1))
+        {
+            Mkdir $param1
+        }
+    }
+    catch
+    {
+        Write-host "$Error[0]"
+        ForceErr
+    }
+   
+}
+
+function DelFile ($param1)
+{
+    try
+    {
+        if ((Test-Path $param1))
+        {
+            Remove-Item -Path $param1 -Force
+        }
+    }
+    catch
+    {
+        Write-host "$Error[0]"
+        ForceErr
+    }
+   
+}
+
+function AutoPilot ($param1,$param2)
+{
+    try
+    {
+       $msg = $param1 + " " + $param2
+       Out-File -FilePath $LogAuto -InputObject $msg -Append -Force
+    }
+    catch
+    {
+        Write-host "$Error[0]"
+        ForceErr
+    }
+   
+}
+
+function Manufacturer
+{
+$Manufacturer = ((Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer).toupper()
+
+if ($Manufacturer -eq 'HEWLETT-PACKARD')
+{
+$Manufacturer = 'HP'
+}
+
+write-host $Manufacturer -ForegroundColor Green
+
+Return $Manufacturer
+}
+
+function ComputerModel
+{
+
+    # Get Model
+    $Model = (Get-CimInstance -ClassName CIM_ComputerSystem -ErrorAction SilentlyContinue -Verbose:$false).Model
+    $Model = $Model.Trim()
+
+    if ($Model.Length -gt 5) 
+    {
+        $Model = $Model.Substring(0, 4)
+    }
+
+    if ($Model -notmatch '^\w{4,5}$') 
+    {
+        throw "Could not parse computer model number. This may not be a Lenovo computer, or an unsupported model."
+    }
+   
+    Return $Model  
+}
+
+function GiveRights ($folder, $user)
+{
+
+
+    Write-Host "Set File Permission to $folder"
+    $ACL = Get-ACL -Path $Folder
+    
+    $user = New-Object -TypeName 'System.Security.Principal.SecurityIdentifier' -ArgumentList @([System.Security.Principal.WellKnownSidType]::AuthenticatedUserSid, $null)
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($user, 'Read', 'ContainerInherit,ObjectInherit', 'None', 'Allow')
+    $acl.SetAccessRule($rule)
+    Set-Acl -Path $folder -AclObject $acl
+
+}
+
+function CheckPermission ($folder, $user)
+{
+    $getuser = 'NT AUTHORITY\' + $user
+
+    $setpermission = $true
+
+    $permission = (Get-Acl $Folder).Access | ?{$_.IdentityReference -match $User} | Select IdentityReference,FileSystemRights
+
+    If ($permission)
+    {
+        foreach ($sec in $permission)
+        {
+            if ($sec.FileSystemRights -notlike "-*")
+            {
+                [string]$VerifyPermission = $sec.FileSystemRights
+                if (($VerifyPermission.ToUpper()).contains("MODIFY") -or ($VerifyPermission.ToUpper()).contains("FULLCONTROL"))
+                {
+                    $setpermission = $false              
+                }
+            }      
+        }
+    }
+
+    Return $setpermission
+}
+
+# If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
+if ("$env:PROCESSOR_ARCHITECTURE" -ne "ARM64")
+{
+    if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe")
+    {
+        & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy bypass -NoProfile -File "$PSCommandPath" -Reboot $Reboot -RebootTimeout $RebootTimeout
+        Exit $lastexitcode
+    }
+}
+
+# Start logging
+$LogAuto = "C:\Windows\Temp\Logs\AutoPilot\HPAC.log"
+$DirAuto = "C:\Windows\Temp\Logs\AutoPilot"
+$LogFile = "C:\Windows\Temp\Logs\HPAC\PS_HPAC.log"
+$LogErr = "C:\Windows\Temp\Logs\HPAC\PS_HPAC.nok"
+$LogDir = "C:\Windows\Temp\Logs\HPAC"
+$tempdirectory = "C:\Windows\Temp\HPAC"
+$folder = "C:\Program Files\WindowsApps\AD2F1837.HPAccessoryCenter_2.16.3374.0_x64__v10z8vjag6ke6"
+
+
+CreateDir $LogDir
+CreateDir $DirAuto
+CreateDir $tempdirectory
+
+DelFile $LogFile
+DelFile $LogErr
+
+Start-Transcript $LogFile
+#$DebugPreference = 'Continue'
+#$VerbosePreference = 'Continue'
+#$InformationPreference = 'Continue'
+
+Write-Host "Begin"
+Write-Host $scriptVersion
+
+$now = Get-Date -Format "dd-MM-yyyy hh:mm:ss"
+AutoPilot "Begin" $now
+
+$Manufacturer = Manufacturer
+
+if ($Manufacturer -eq 'HP')
+{
+
+    try
+    {
+        # Copy the files
+        write-host "Copy Files"
+        Copy-Item ".\files\*" $tempdirectory -Force -Recurse  
+   
+        $vclibs = Join-Path $tempdirectory -ChildPath "Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64__8wekyb3d8bbwe.Appx"
+        $hpac = Join-Path $tempdirectory -ChildPath "AD2F1837.HPAccessoryCenter_2.16.3374.0_neutral_~_v10z8vjag6ke6.AppxBundle"
+        write-host "Install HP Accessory Center"
+        # winget install "HP Accessory Center" --accept-source-agreements --accept-package-agreements --scope machine --source msstore
+        
+        Add-AppProvisionedPackage -Online -PackagePath $vclibs -SkipLicense
+        Add-AppProvisionedPackage -Online -PackagePath $hpac -SkipLicense
+
+        Add-AppPackage -Path $vclibs
+        Add-AppPackage -Path $hpac
+
+        # Load Default User Profile to registry
+
+        reg.exe load HKLM\TempUser "C:\Users\Default\NTUSER.DAT" | Out-Host
+
+        # Get Reg Files
+        $List = Get-ChildItem -Path $TempDirectory -filter "*.reg"
+
+        # Process List of Reg Files
+        foreach ($item in $list)
+        {
+            try
+            {
+                $Name = ($item.Name).Split(".")[0]
+                $Command = "c:\windows\regedit.exe"
+                $parm = "/S " + $item.FullName
+                $run = (Start-Process $Command -ArgumentList $parm -Wait -PassThru)
+            }
+            catch
+            {
+                Write-host "$Error[0]"
+                ForceErr
+            }
+        }
+
+        # Unload Default User Profile from registry
+        reg.exe unload HKLM\TempUser | Out-Host
+    }
+    catch
+    {
+        Write-host "$Error[0]"
+        ForceErr  
+    }
+}
+Else
+{
+    write-host "Nothing to do not a HP Model"
+}
+
+
+
+write-host "Clean Up"
+    
+Set-Location c:\
+	
+if ((Test-Path -Path $TempDirectory))
+{
+    Remove-Item -Path $TempDirectory -Force -Recurse
+}
+
+
+$now = Get-Date -Format "dd-MM-yyyy hh:mm:ss"
+AutoPilot "End  " $now
+$intunelog = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\IntuneManagementExtension-" + $LogFile.Split("\")[-1]
+Copy-Item $LogFile $intunelog -Force -ErrorAction SilentlyContinue
+Stop-Transcript
+
---
+++ Sith_UpdateDrv.ps1
@@ -0,0 +1,210 @@
+﻿Remove-Variable * -ErrorAction SilentlyContinue
+
+$scriptVersion = "20250122"
+
+function ForceErr
+{
+    Stop-Transcript
+    Rename-Item -Path $LogFile -NewName $LogErr -Force
+    
+    if ((Test-Path $LogFile))
+    {
+        Remove-Item -Path $LogFile -Force
+    }
+
+    $intunelogerr = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\IntuneManagementExtension-" + $Logerr.Split("\")[-1] + ".log"
+    Copy-Item $Logerr $intunelogerr -Force -ErrorAction SilentlyContinue
+    
+
+    exit 13    
+}
+
+function CreateDir ($param1)
+{
+    try
+    {
+        if (-not (Test-Path $param1))
+        {
+            Mkdir $param1
+        }
+    }
+    catch
+    {
+        Write-host "$Error[0]"
+        ForceErr
+    }
+   
+}
+
+function DelFile ($param1)
+{
+    try
+    {
+        if ((Test-Path $param1))
+        {
+            Remove-Item -Path $param1 -Force
+        }
+    }
+    catch
+    {
+        Write-host "$Error[0]"
+        ForceErr
+    }
+   
+}
+
+function AutoPilot ($param1,$param2)
+{
+    try
+    {
+       $msg = $param1 + " " + $param2
+       Out-File -FilePath $LogAuto -InputObject $msg -Append -Force
+    }
+    catch
+    {
+        Write-host "$Error[0]"
+        ForceErr
+    }
+   
+}
+
+# If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
+if ("$env:PROCESSOR_ARCHITECTURE" -ne "ARM64")
+{
+    if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe")
+    {
+        & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy bypass -NoProfile -File "$PSCommandPath" -Reboot $Reboot -RebootTimeout $RebootTimeout
+        Exit $lastexitcode
+    }
+}
+
+# Start logging
+$LogAuto = "C:\Windows\Temp\Logs\AutoPilot\UpdateOS_DRV.log"
+$DirAuto = "C:\Windows\Temp\Logs\AutoPilot"
+$LogFile = "C:\Windows\Temp\Logs\UpdateOS\PS_UpdateOS_DRV.log"
+$LogErr = "C:\Windows\Temp\Logs\UpdateOS\PS_UpdateOS_DRV.nok"
+$LogDir = "C:\Windows\Temp\Logs\UpdateOS"
+
+
+CreateDir $LogDir
+
+DelFile $LogFile
+DelFile $LogErr
+
+Start-Transcript $LogFile
+#$DebugPreference = 'Continue'
+#$VerbosePreference = 'Continue'
+#$InformationPreference = 'Continue'
+
+Write-Host "Begin"
+Write-Host $scriptVersion
+
+try
+{
+    # Reset WindowsUpdate
+    # Reset-WUComponents
+    # Load module from PowerShell Gallery
+    Import-Module PSWindowsUpdate
+}
+catch
+{
+    write-output "Error install PowerShell Module PSWindowsUpdate"
+    ForceErr
+}
+
+# Install all available updates
+$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+Write-Output "$ts Installing updates."
+try
+{   
+    $GETUPDATES = Get-WindowsUpdate -AcceptAll -WindowsUpdate -UpdateType Driver -IgnoreReboot
+    $CheckNumberUpdates = $GETUPDATES.Count
+    $UpdatetobeInstall = $GETUPDATES | select Title
+    Write-Output "Detect $CheckNumberUpdates Updates"
+    Write-Output $UpdatetobeInstall
+
+    $Reboot = 'None'
+    Write-Output $Reboot
+}
+catch
+{
+    write-output "Error getting Updates"
+    ForceErr
+}
+
+
+try
+{
+    if ($CheckNumberUpdates -ne 0)
+    {
+        foreach ($item in $GETUPDATES)
+        {      
+            $updateid = $item.Title
+            $updatereboot = $item.RebootRequired
+            $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"               
+            Write-Output "$ts Start"
+            Write-Output "$updateid is installing"
+            Write-Output $updatereboot
+            if ($item.RebootRequired -EQ $True)
+            {
+                $Reboot = $true
+                Write-Output "Change Reboot Variable to $Reboot"
+            }
+            Install-WindowsUpdate -WindowsUpdate -AcceptAll -IgnoreReboot -Verbose -UpdateID ($item.Identity).UpdateID -ErrorAction SilentlyContinue | Select Title, KB, Result | Format-Table
+            Start-Sleep -Seconds 7
+    }
+    Else
+    {
+        write-host "No updates available"
+    }
+}
+}
+catch
+{
+    Write-Output "error install update $updateid"
+    ForceErr
+}
+
+
+try
+{    
+    $version = "0003"
+    $VersionLocation = "HKLM:\SOFTWARE\MillenniumBCP\Intune"
+
+
+    if(-NOT (Test-Path $VersionLocation))
+    { 
+        if(-NOT(Test-Path "HKLM:\SOFTWARE\MillenniumBCP"))
+        {
+            New-Item "HKLM:\SOFTWARE" -Name "MillenniumBCP"
+        }
+
+        if(-NOT(Test-Path "HKLM:\SOFTWARE\MillenniumBCP\Intune"))
+        {
+            New-Item "HKLM:\SOFTWARE\MillenniumBCP" -Name "Intune"
+        }
+    }
+
+    Set-ItemProperty -Path $VersionLocation -Name "WindowsUpdateDriver" -Value $version
+}
+catch
+{
+    Write-Output "Erro creating registry mark"
+    ForceErr
+}
+
+
+$intunelog = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\IntuneManagementExtension-" + $LogFile.Split("\")[-1]
+Copy-Item $LogFile $intunelog -Force -ErrorAction SilentlyContinue
+Stop-Transcript
+
+if ($CheckNumberUpdates -ne 0)
+{   
+    Exit 1641
+}
+else
+{   
+    Exit 0
+}
+
+
